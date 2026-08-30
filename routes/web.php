33f34f2e6b4f2f718468f4112ff41b0a9d6b6467
VerdadDev <aldousverdad@gmail.com>
2026-08-31 00:32:12 +0800

    add detail page with route parameter and 404 handling

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/comics/filter', [ComicController::class, 'filter'])->name('comics.f
 
 Route::get('/comics/filter/{genre?}', [ComicController::class, 'filter'])->name('comics.filter');
 
-Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
+Route::get('/mangas', [ComicController::class, 'index'])->name('comics.index');
 
 Route::get('/comics/{id?}', [ComicController::class, 'show'])->name('comics.show');
 
